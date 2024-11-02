bd032cc5000eb6052199d1bb9c214faa1048b1bf
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-11-02 11:33:07 +0400

    js

---
+++ pure-js/lesson1-introduction/js/script.js
@@ -0,0 +1,49 @@
+// // console.log("new Hello world");
+
+// // Primitive DT
+// // String
+// var infoOne = "Hello World";
+// console.log(infoOne);
+
+// // Number
+// var infoTwo = 5;
+// console.log(infoTwo);
+
+// // Boolean
+// var infoThree = true;
+// // false | true;
+// console.log(infoThree);
+
+// // Composite DT 
+// // Array
+// var infoFour =  [true,5,"Hello"];
+// console.log(infoFour);
+
+// // Object
+// var infoFive = {
+//     name:"Resul",
+//     age:20
+// }
+// console.log(infoFive);
+
+// // Special DT
+// // undefined
+// var infoSix = undefined;
+// console.log(infoSix);
+
+// // null
+//  var infoSeven = null;
+//  console.log(infoSeven);
+
+//  var a;
+//  console.log(a);
+
+// debugger;
+// consolea.log("Hello my data")
+// console.log("Hello my data")
+//  console.log("Hello my data")
+//  console.log("Hello my data")
+//  console.log("Hello my data")
+ 
+
+
