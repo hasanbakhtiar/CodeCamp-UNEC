2bbbdd4dca0d3938c8e8b63cb927c046a527b2d0
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-11-11 20:08:18 +0400

    lastest

---
+++ pure-js/lesson7-function/js/script.js
@@ -0,0 +1,109 @@
+// function name (params){
+    // code
+// }
+
+
+// function info(){
+    
+//     console.log("Hello World");
+// }
+
+// info();
+
+
+// function mydata(dt){
+//     return dt;
+// }
+
+// function info(){
+//     return mydata([1,2,3,4,5]);
+// }
+// console.log(info());
+
+// console.log(calculateSpeed(300,2));
+// console.log(calculateSpeed(200,3));
+
+// function calculateSpeed(km,hour){
+//         return "Current speed: "+(km/hour) +"km/h";
+// }
+
+
+
+// const info = function(){
+    //     console.log("Hello World");
+    // }
+    
+    // info();
+
+    // IIFE 
+// (function(a,b){
+//     console.log(a+b);
+// })(10,20)
+
+// ()=>{} 
+
+
+    
+    
+// const info = (a)=>{
+//     console.log("Hello World");
+// }
+// info(()=>{})
+// let info = "Hello world";
+// let result = info.length;
+// console.log(result);
+
+
+
+// let info = ["Table","Pen","Book"];
+// info.push("New Value");
+// info.pop();
+// info.shift();
+// info.unshift("New value")
+
+
+// console.log(info);
+
+
+// const myFunc = (comingData)=>{
+//     let data = comingData - 50;
+//     if (data<100) {
+//         return "Success"
+        
+//     }else{
+//         return "Wrong";
+//     }
+// }
+
+// console.log(myFunc(30));
+
+
+
+
+import {data} from './data.js';
+
+
+// data.products.map((item,index)=>{
+//     console.log(item.title);
+    
+// })
+
+
+
+// data.products.forEach((item,index)=>{
+//     console.log(item.title);
+// })
+
+
+
+// const filteredData = data.products.filter((item)=>(item.category === "beauty"))
+// console.log(filteredData);
+
+
+// const findData = data.products.find((item)=>(item.category === "beauty"))
+// console.log(findData);
+
+
+
+
+
