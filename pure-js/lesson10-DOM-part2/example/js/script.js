74a10420875673a483335d964d92ab233cfe6fbb
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-11-18 20:37:18 +0400

    dom part 2

---
+++ pure-js/lesson10-DOM-part2/example/js/script.js
@@ -0,0 +1,7 @@
+const nav  = document.querySelector('nav');
+const modeBtn = document.querySelector('#mode-btn');
+
+modeBtn.onclick = ()=>{
+    nav.className = "navbar navbar-expand-lg bg-dark navbar-dark"
+    
+}
---
+++ pure-js/lesson10-DOM-part2/js/script.js
@@ -0,0 +1,63 @@
+
+
+
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+
+
+btn.onclick = ()=>{
+    // h1.attributes[0].value = 'green a'
+    // h1.className = "green"
+    // h1.classList.add('green',"red")
+    // h1.classList.add('red')
+
+    // h1.classList.remove('green')
+    h1.classList.toggle('hide')
+
+    
+}
+
+
+
+
+
+// const input = document.querySelector('input');
+
+// input.onkeyup = ()=>{
+//     input.style.height = '100px';
+//     input.style.transition = '.5s';
+//     input.style.backgroundColor = "white";
+//     input.style.color = "black";
+// }
+// input.onkeydown = ()=>{
+//     input.style.transition = '.5s';
+//     input.style.height = '200px';
+//     input.style.backgroundColor = "black";
+//     input.style.color = "white";
+
+// }
+
+
+
+// btn.onmouseup=()=>{
+//     h1.innerHTML = "Hello"
+// }
+
+// btn.onmousedown=()=>{
+//     h1.innerHTML = "Test"
+// }
+
+
+
+
+// btn.addEventListener('click',()=>{
+//         h1.innerHTML = "Test"
+// },true)
+
+
+// btn.onclick=()=>{
+    
+//     console.log('test');
+// };
+
