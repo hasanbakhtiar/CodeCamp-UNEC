7a75d2c40b07c622010bd6b48895906dc0d698ed
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-11-25 20:44:42 +0400

    laste

---
+++ pure-js/lesson11-localStoregae/js/script.js
@@ -0,0 +1,40 @@
+// localStorage.setItem();
+// localStorage.getItem();
+// localStorage.setItem('name',"Hasan");
+// localStorage.setItem('surname',"Bakhtiar");
+// // localStorage.removeItem('name')
+// localStorage.clear();
+
+// document.querySelector('h1').innerHTML = localStorage.getItem("name")
+// document.querySelector('p').innerHTML = localStorage.getItem("surname")
+
+
+
+// const test = [
+//     {
+//         id:1,
+//         title:"Hello1"
+//     },
+//     {
+//         id:2,
+//         title:"Hello2"
+//     },
+//     {
+//         id:3,
+//         title:"Hello3"
+//     }
+// ]
+
+// const covertData = JSON.stringify(test);
+
+
+// localStorage.setItem('data',covertData);
+
+
+// const comingData = JSON.parse(localStorage.getItem('data'));
+
+// console.log(comingData[0].title);
+
+// document.querySelector('h1').innerHTML = 
+
+
---
+++ pure-js/lesson12-BOM-part1/js/script.js
@@ -0,0 +1,10 @@
+document.querySelector("button").onclick=()=>{
+    window.open("https://google.com","","width=300,height=300")
+}
+
+
+// console.log(window.navigator.language);
+
+
+
+
